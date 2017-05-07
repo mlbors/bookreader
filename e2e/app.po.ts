85b3a4ac05f51325368266eb8442f790bf65826e
mlbors <matyas.bors@gmail.com>
2017-05-07 17:58:55 +0200

    Change app.po.ts

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -2,7 +2,7 @@ import { browser, element, by } from 'protractor';
 
 export class BookreaderPage {
   navigateTo() {
-    return browser.get('/');
+    return browser.get('/bookreader');
   }
 
   getParagraphText() {
